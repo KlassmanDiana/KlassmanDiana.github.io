3beecf1a27a7eb647007f034f5580b6bc69477df
Klassman <vk.dik@yandex.ru>
2020-02-17 22:40:29 +0300

    Mini-shop_Aliexpress

---
+++ Mini-Shop_Ozon/src/modules/filter.js
@@ -0,0 +1,26 @@
+
+    export default function filter() {
+        const checkbox = document.getElementById('discount-checkbox');
+        const cards = document.querySelectorAll('.goods .card');
+        const min = document.getElementById('min');
+        const max = document.getElementById('max');
+        const activeLi = document.querySelector('.catalog-list li.active');
+
+        cards.forEach((card) => {
+            const cardPrice = card.querySelector('.card-price');
+            const price = parseFloat(cardPrice.textContent);
+            const discount = card.querySelector('.card-sale');
+
+            card.parentNode.style.display = ''
+
+            if ((min.value && price < min.value) || (price > max.value && max.value)) {
+                card.parentNode.style.display = 'none'
+            } else if (checkbox.checked && !discount) {
+                card.parentNode.style.display = 'none'
+            } else if (activeLi) {
+                if (card.dataset.category !== activeLi.textContent) {
+                    card.parentNode.style.display = 'none'
+                }
+            }
+        })
+    };
